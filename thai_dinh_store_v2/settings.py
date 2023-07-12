c301ab297ea6562ead9844fb536294dca04bd5f8
MrNoname2k <bimzc123@gmail.com>
2023-07-12 18:47:31 +0700

    b

--- thai_dinh_store_v2/settings.py
+++ thai_dinh_store_v2/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
+print(f'{ BASE_DIR } ddang dduowjc in ra ma hinh')
 
 
 
