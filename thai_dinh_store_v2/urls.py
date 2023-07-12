dec628e2e9ed3a157077548cc486bac38c0aee25
MrNoname2k <bimzc123@gmail.com>
2023-07-12 21:22:14 +0700

    ok3

--- thai_dinh_store_v2/urls.py
+++ thai_dinh_store_v2/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('loginAndRegister.urls')),
     path('',include('shop.urls'))
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 # +staticfiles_urlpatterns()
 # +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
